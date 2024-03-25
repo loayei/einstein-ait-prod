1fdb2f89081d5375a3834ba1b84fdfa8e53440c0
loayei <loayei@gmail.com>
2024-03-25 15:51:37 -0700

    claude

--- neurons/miners/openai/miner.py
+++ neurons/miners/openai/miner.py
@@ -11,6 +11,7 @@ from einstein.protocol import CoreSynapse
 from neurons.miner import Miner
 
 from langchain.prompts import ChatPromptTemplate
+from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
 from langchain_community.callbacks import get_openai_callback
 from dotenv import load_dotenv, find_dotenv
@@ -24,8 +25,8 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-class OpenAIMiner(Miner):
-    """Langchain-based miner which uses OpenAI's API as the LLM.
+class ClaudeMiner(Miner):
+    """Langchain-based miner which uses Anthropic's Claude API as the LLM.
 
     You should also install the dependencies for this miner, which can be found in the requirements.txt file in this directory.
     """
@@ -33,7 +34,7 @@ class OpenAIMiner(Miner):
     @classmethod
     def add_args(cls, parser: argparse.ArgumentParser):
         """
-        Adds OpenAI-specific arguments to the command line parser.
+        Adds Claude-specific arguments to the command line parser.
         """
         super().add_args(parser)
 
@@ -41,39 +42,39 @@ class OpenAIMiner(Miner):
         super().__init__(config=config)
 
         bt.logging.info(f"Initializing with model {self.config.neuron.model_id}...")
-        
+
         if not self.config.numpal.off:
             bt.logging.info("⚡️ \033[1;33mSupercharging the model with NumPAL...\033[0m")
         else:
             bt.logging.info(f"NumPAL is turned off...")
-        
+
         if not self.config.numpal.verbose.off:
             bt.logging.info(f"NumPAL verbose mode is turned on...")
         else:
             bt.logging.info(f"NumPAL verbose mode is turned off...")
-        
+
         if self.config.wandb.on:
-            self.identity_tags = ("openai_miner",) + (self.config.neuron.model_id,)
+            self.identity_tags = ("claude_miner",) + (self.config.neuron.model_id,)
 
         _ = load_dotenv(find_dotenv())
-        api_key = os.environ.get("OPENAI_API_KEY")
+        api_key = os.environ.get("ANTHROPIC_API_KEY")
 
-        # Set openai key and other args
-        self.model = ChatOpenAI(
+        # Set Claude key and other args
+        self.model = ChatAnthropic(
             api_key=api_key,
-            model_name=self.config.neuron.model_id,
-            max_tokens=self.config.neuron.max_tokens,
+            model=self.config.neuron.model_id,
+            max_tokens_to_sample=self.config.neuron.max_tokens,
             temperature=self.config.neuron.temperature,
         )
-        
+
         system_prompt = self.config.neuron.system_prompt
         self.system_prompt = system_prompt + """\nMandatory:
         - If the answer is a symbol, you must say 'So the final answer is: (that symbol)'.
         - Unless not symbol, you always end the entire sentence with 'So the final answer is: (the answer)'
         """
-        
+
         bt.logging.info(f'Your current system prompt is: {self.system_prompt}')
-        
+
         self.accumulated_total_tokens = 0
         self.accumulated_prompt_tokens = 0
         self.accumulated_completion_tokens = 0
@@ -119,39 +120,39 @@ class OpenAIMiner(Miner):
             # with get_openai_callback() as cb:
             t0 = time.time()
             bt.logging.debug(f"📧 Message received, forwarding synapse: {synapse}")
-            
+
             # Create a chain of operations to process the input
             prompt = ChatPromptTemplate.from_messages(
                 [("system", self.system_prompt), ("user", "{input}")]
-                )
-            
+            )
+
             chain = prompt | self.model | StrOutputParser()
-            
+
             role = synapse.roles[-1]
-            
+
             # Get the math question from the last message
             math_question = synapse.messages[-1]
-            
+
             # If NumPAL is turned on, use it to process the math question
             if not self.config.numpal.off:
 
                 bt.logging.debug("\033[1;32m💬 Running Math script on NumPAL\033[0m")
                 verbose_on = not self.config.numpal.verbose.off
-                
+
                 q_r = NumPAL.from_math_prompt(self.model, verbose=verbose_on).invoke(math_question)
-                
+
                 response = chain.invoke({"role": role, "input": str(q_r)})
 
             # If NumPAL is turned off, use the model to process the math question
             else:
 
-                bt.logging.debug(f"💬 Querying OpenAI...")
+                bt.logging.debug(f"💬 Querying Claude...")
 
                 response = chain.invoke({"role": role, "input": math_question})
 
             synapse.completion = response
             synapse_latency = time.time() - t0
-            
+
             bt.logging.info(f'📧 \033[1;34mMessage received: {math_question}\033[0m')
             bt.logging.info(f'📧 \033[1;34mResponse: {response}\033[0m')
 
